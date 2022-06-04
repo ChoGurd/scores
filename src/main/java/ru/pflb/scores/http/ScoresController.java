e202bedfca97a9b6d4f16d515ca23b5b1f666d27
ChoGurd <skobelevee@mail.ru>
2022-06-04 21:46:07 +0400

    Add Method postRecord and getHighScores in class ScoresController

--- src/main/java/ru/pflb/scores/http/ScoresController.java
+++ src/main/java/ru/pflb/scores/http/ScoresController.java
@@ -1,17 +1,33 @@
 package ru.pflb.scores.http;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.pflb.scores.dto.Score;
+import ru.pflb.scores.service.ScoreService;
 
 @RestController
 public class ScoresController {
 
 
+    private final ScoreService scoreService;
+
+    @Autowired // не обязательно
+    public ScoresController(ScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
+
+
     @GetMapping(path = "/hiscores/{levelId}")
     //TODO implement
+    public String getHighScores(@PathVariable int levelId) {
+        return scoreService.highScores(levelId);
+    }
+
 
     @PostMapping(path = "/score/{levelId}/{userId}")
     //TODO implement; pass sessionKey as Query Param (/.../../..?session={sessionKey})
+    public void postRecord(@PathVariable int levelId, @PathVariable int userId, @RequestParam("session") String sessionKey, @RequestBody String score)
+    {
+        scoreService.record(sessionKey, Integer.parseInt(score), levelId);
+    }
 }
